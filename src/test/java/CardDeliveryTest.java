2002b1da969068aa690187eae735e0d77de1376d
Odium-Mundi <violetta_wolf@mail.ru>
2020-07-17 03:01:11 +0300

    first test

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    @Test
+    void shouldReservationCardWhenValidData() {
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue("Краснодар");
+        $("[placeholder='Дата встречи'").sendKeys(Keys.LEFT_SHIFT, Keys.HOME, Keys.DELETE);
+        $("[placeholder='Дата встречи'").setValue("30092020");
+        $("[data-test-id=\"name\"] .input__control").setValue("Паршикова Виолетта");
+        $("[name='phone']").setValue("+79200000000");
+        $(".checkbox__box").click();
+        $(byText("Забронировать")).click();
+        $(withText("Успешная авторизация")).waitUntil(Condition.visible, 15000);
+        // TODO:
+    }
+}
